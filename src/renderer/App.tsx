0aa4bdbe0f5daf34fa62f29f0e6e967c3a1e3c66
Mateusz Wiącek <68820849+MarioPeperoni@users.noreply.github.com>
2026-08-22 15:50:27 +0200

    deps: setup react

---
+++ src/renderer/App.tsx
@@ -0,0 +1,11 @@
+import { Toolbar } from './components/Toolbar';
+import { Canvas } from './components/Canvas';
+
+export const App = () => {
+  return (
+    <div className="app">
+      <Toolbar />
+      <Canvas />
+    </div>
+  );
+};
